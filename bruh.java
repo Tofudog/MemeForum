88fe56f288eeea2d4c089c7df143909d62572786
Tofudog <leodefarias25@gmail.com>
2024-04-06 14:43:45 -0500

    added bruh!

--- bruh.java
+++ bruh.java
@@ -1,4 +1,15 @@
+//this is evidently the most impressive work of art...
+//this simple class does more work than the entirety of Microsoft!
 public class bruh {
+	
+	private int doi;
+	private char let;
+	
+	public bruh(int doi, char let) {
+		this.doi = doi;
+		this.let = let;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("BRUH!");
 	}
