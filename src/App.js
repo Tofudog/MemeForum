021f2c251284fe73520127a1e1cd3c9eeb9bbefa
Tofudog <leodefarias25@gmail.com>
2024-05-03 22:48:56 -0500

    made small progress

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import { useState } from 'react';
 
 /*
 Report of current problems:
-  --> Button click affects other clicks
   --> counter h2 is not side by side to meme
+  --> currently, 'votes' field in each meme is useless
+
+To do:
+  --> cap the number of likes user can do
+  --> include a 'form' that allows choosing meme category
+    ---> whenever new category is chosen, re-render entire page
+    ---> use hashmap to id the memes (remember the cur problem 'votes' field)
+    ---> create another function to loop through memes and render accordingly
+      ----> cannot be in 'default App()'
+      ----> make sure 'like' functions are available
+    ---> perhaps the memes should be a global variable
 */
 
 export default function App() {
@@ -14,36 +24,56 @@ export default function App() {
   const meme1 = {
     file: "https://wallpapercave.com/wp/wp7474251.jpg",
     votes: 3,
-    imageSize: 140,
+    imageSize: 180,
   }
   const meme2 = {
     file: "https://tse4.mm.bing.net/th?id=OIP.1Uu2KP7LqkHsiEaT2xsJJgHaGl&pid=Api&P=0&h=220.jpg",
     votes: 3,
-    imageSize: 140,
+    imageSize: 180,
   }
 
-  const [counts, setCount] = useState([0, 0]);
+  //'counts' represents the global net votes on a meme
+  //'curv' represents user votes, which limits a user
+  //...to have a net vote of [-5,5]
+  const [counts, setCount] = useState([meme1.votes, meme2.votes]);
+  const [curv, setCurv] = useState([0, 0]);
 
   function likeMeme(idx) {
     const nextCounters = counts.map((c, i) => {
-      if (i === idx) {
+      if (i === idx && curv[i] !== 5) {
+        //the index is matched and the user votes does exceed net 5
         return c + 1;
       } else {
         return c;
       }
     });
     setCount(nextCounters);
+    const nextCurv = curv.map((c, i) => {
+      if (i === idx & curv[i] !== 5) {
+        //the index is matched and the user votes does exceed net 5
+        return c + 1;
+      } else {return c;}
+    })
+    setCurv(nextCurv);
   }
   
   function unlikeMeme(idx) {
     const nextCounters = counts.map((c, i) => {
-      if (i === idx) {
+      if (i === idx && curv[i] !== -5) {
+        //the index is matched and the user votes does exceed net -5
         return c - 1;
       } else {
         return c;
       }
     });
     setCount(nextCounters);
+    const nextCurv = curv.map((c, i) => {
+      if (i === idx & curv[i] !== -5) {
+        //the index is matched and the user votes does exceed net -5
+        return c - 1;
+      } else {return c;}
+    })
+    setCurv(nextCurv);
   }
 
   return (
